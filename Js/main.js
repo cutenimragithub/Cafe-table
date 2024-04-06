f51ec101f41cd552b587cc7312d3a57bbdf65e85
unknown <nimrakhatoon6648@gmail.com>
2024-04-06 17:24:21 +0500

    some changes

--- Js/main.js
+++ Js/main.js
@@ -1,36 +1,4 @@
-// Header Scroll
-let nav = document.querySelector(".navbar");
-window.onscroll = function (){
-    if(document.documentElement.scrollTop > 50){
-        nav.classList.add("header-scrolled");
-    }else{
-        nav.classList.remove("header-scrolled");
-    }
-}
 
-// nav Hide
-let navbar = document.querySelectorAll(".nav-link");
-let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navbar.forEach(function(a){
-    a.addEventListener("click", function(){
-        navCollapse.classList.remove("show");
-    })
-})
-// Swiper Slider
-const swiper = new Swiper('.my-swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay:{
-        delay: 3500,
-    },
-  });
 
 
 
