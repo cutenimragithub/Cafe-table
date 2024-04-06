5f2751edb49d583ac382891bf6d979184f1b0f96
unknown <nimrakhatoon6648@gmail.com>
2024-04-06 16:32:33 +0500

    first commit

--- Js/main.js
+++ Js/main.js
@@ -31,3 +31,26 @@ const swiper = new Swiper('.my-swiper', {
         delay: 3500,
     },
   });
+
+
+
+// This script switches the table when the carousel slides
+var myCarousel = document.querySelector('#coffeeCarousel');
+
+myCarousel.addEventListener('slide.bs.carousel', function (e) {
+  // Get the index of the next slide
+  var nextSlideIndex = e.relatedTarget.getAttribute('data-bs-slide-number');
+
+  // Hide all tables
+  var tables = document.querySelectorAll('.pricing-table');
+  tables.forEach(function (table) {
+    table.classList.add('hidden');
+  });
+
+  // Show the table corresponding to the new carousel item
+  var nextTable = document.querySelector('#menuTable' + nextSlideIndex);
+  if (nextTable) {
+    nextTable.classList.remove('hidden');
+  }
+});
+
